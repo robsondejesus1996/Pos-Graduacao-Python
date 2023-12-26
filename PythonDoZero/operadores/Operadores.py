1e2ce28a9bb807b38acb5e2ba83c371cd68da5eb
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-12-26 11:40:47 -0300

    Tipos de Operadores

---
+++ PythonDoZero/operadores/Operadores.py
@@ -0,0 +1,3 @@
+calculo = (2 + 2) ** 3
+
+print(calculo)
---
+++ PythonDoZero/operadores/OperadoresComparacao.py
@@ -0,0 +1,22 @@
+operadores1 = 10 == 10
+print(operadores1)
+
+operadores2 = 'banana' == 'Banana'
+print(operadores2)
+
+
+operadores3 = 'banana' != 'Banana';
+print(operadores3)
+
+
+operadores4 = 10 > 13
+print(operadores4)
+
+operadores5 = 10 < 9
+print(operadores5)
+
+operadores6 = 10 <= 9
+print(operadores6)
+
+operadores7 = 10 >= 9
+print(operadores7)
